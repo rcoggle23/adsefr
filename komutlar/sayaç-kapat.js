f1c8f54a77421c76b379a247decf4f9b471b7573
Glitch (maroon-crimson-iodine) <none>
2023-05-22 15:27:00 +0000

    🌄🍪 Checkpoint ./komutlar/sayaç-kapat.js:49830958/76 ./komutlar/sunucu-duyuru.js:49830958/25 ./komutlar/sunucu-oylama.js:49830958/25 ./komutlar/yardım-davetsistemi.js:49830958/50

--- komutlar/sayaç-kapat.js
+++ komutlar/sayaç-kapat.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const db = require('quick.db')
 exports.run = (client, message, args) => { 
 
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`<:a_:1109866752428490974>  **Bu komutu kullanabilmek için** "\`Yönetici\`" **yetkisine sahip olmalısın.**`);
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`  **Bu komutu kullanabilmek için** "\`Yönetici\`" **yetkisine sahip olmalısın.**`);
  const rol = db.fetch(`sayacS_${message.guild.id}`)  
- if(!rol) return message.reply(`<:a_:1109866752428490974>   **Bu özellik zaten kapalı! :wink: **`)
+ if(!rol) return message.reply(`   **Bu özellik zaten kapalı! :wink: **`)
  
  
-  message.channel.send(`<:a_:1109866752428490974>   **Sayaç başarılı bir şekilde kapatıldı.**`)
+  message.channel.send(`  **Sayaç başarılı bir şekilde kapatıldı.**`)
 
  
   db.delete(`sayacS_${message.guild.id}`)  
